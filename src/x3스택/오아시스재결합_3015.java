c976d87b0998dd0dfa8926e88c91d4f5b89d1564
kangchanwook <kan0202@naver.com>
2024-09-11 17:21:31 +0900

    [SOLVED] : 큐_큐_10845
    comment: -
    knowledge: Dequeue를 Queue 로 사용하려면 push() 와 pollLast() 를 사용하면 된다.
    peek(), getLast() 도 사용 고려.

--- src/x3스택/오아시스재결합_3015.java
+++ src/x3스택/오아시스재결합_3015.java
@@ -1,5 +1,7 @@
 package x3스택;
 
+import jdk.jfr.Unsigned;
+
 import java.io.*;
 import java.util.ArrayDeque;
 import java.util.Deque;
@@ -7,88 +9,77 @@ import java.util.Queue;
 
 public class 오아시스재결합_3015 {
     public static void main(String[] args) throws IOException {
-        //Reader, Writer 선언 및 초기 값 읽기
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int temp = 0;
-        int result = 0;
-        Deque<Integer> stack = new ArrayDeque<>();
+
+        Deque<Element> stack = new ArrayDeque<>();
+
+        Long temp = 0L;
+        Long tempPop = -1L;
+        Long result = 0L;
+
 
         for(int i = 0; i < N; i++){
 
-            int presentElement = Integer.parseInt(br.readLine());
-            if(stack.isEmpty()){
-                stack.push(presentElement);
-                continue;
-            }
-            int previousElement = stack.peek();
+            Element presentElement = new Element(Long.parseLong(br.readLine()), false);
 
-            if(presentElement < previousElement){
-
-                result += stack.size() - 1 - temp;
-                temp++;
-                stack.push(presentElement);
-                continue;
-
-            }
-            else if(presentElement > previousElement){
-
-                result++;
-
-                while(true){
-
-                    if(stack.isEmpty()){
-                        stack.push(presentElement);
-                        break;
-                    }
-
-
-                    int peek = stack.peek();
-
-                    if(peek >= presentElement){
-                        stack.push(presentElement);
-                        break;
-                    }
-                    else if(peek < presentElement){
-                        peek = stack.pop();
-
-                        if(stack.isEmpty()){
-                            stack.push(presentElement);
-                            break;
-                        }
-
-                        if(peek < stack.peek()){
-                            temp--;
-                            break;
-
-                        }
-                        else if(peek == stack.peek()){
-                            break;
-                        }
-
-                    }
-
+            while(true){
+                if(stack.isEmpty()){
+                    stack.push(presentElement);
+                    break;
                 }
 
+                Element peekElement = stack.peek();
+
+                if(peekElement.height > presentElement.height){
+                    stack.push(presentElement);
+                    result += stack.size() - 1L - temp;
+                    presentElement.isIncreasedTemp = true;
+                    temp++;
+                    tempPop = -1L;
+                    break;
+                }
+                else if(peekElement.height == presentElement.height){
+                    stack.push(presentElement);
+                    result += stack.size() - 1L;
+                    presentElement.isIncreasedTemp = true;
+                    temp++;
+                    tempPop = -1L;
+                    break;
+                }
+                else{
+                    Element presentPopData = stack.pop();
+
+                    if(tempPop != presentPopData.height)
+                        result += 1L;
+                    if(presentPopData.isIncreasedTemp)
+                        temp--;
+
+                    tempPop = presentPopData.height;
+                }
             }
-            else if(presentElement == previousElement){
-                result += stack.size() - 1 - temp;
-                stack.push(presentElement);
-                continue;
-            }
-
-
-
 
         }
-
 
         bw.write(String.valueOf(result));
         bw.flush();
         bw.close();
         br.close();
+
+    }
+
+    public static class Element{
+        Long height;
+        boolean isIncreasedTemp;
+
+        Element(Long height, boolean isIncreasedTemp){
+            this.height = height;
+            this.isIncreasedTemp = isIncreasedTemp;
+
+        }
 
     }
 }
