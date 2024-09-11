32ef50056e14d22580887e9faf689b417f33f61d
kangchanwook <kan0202@naver.com>
2024-09-11 17:26:26 +0900

    [SOLVED] : 큐_큐2_18258
    comment: -
    knowledge: Dequeue를 Queue 로 사용하려면 push() 와 pollLast() 를 사용하면 된다.
    peek(), getLast() 도 사용 고려.

---
+++ src/x4큐/큐2_18258.java
@@ -0,0 +1,66 @@
+package x4큐;
+
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class 큐2_18258 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<String> queue = new ArrayDeque<>();
+
+        for(int i = 0; i < N; i++){
+            String tempConstruction = br.readLine();
+            int j = 0;
+
+            switch(tempConstruction){
+                case "pop":
+                    if (!queue.isEmpty())
+                        bw.write(queue.pollLast());
+                    else
+                        bw.write("-1");
+                    break;
+                case "size":
+                    bw.write(String.valueOf(queue.size()));
+                    break;
+                case "empty":
+                    if(queue.isEmpty())
+                        bw.write("1");
+                    else
+                        bw.write("0");
+                    break;
+                case "front":
+                    if(queue.isEmpty())
+                        bw.write("-1");
+                    else
+                        bw.write(queue.getLast());
+                    break;
+                case "back":
+                    if(queue.isEmpty())
+                        bw.write("-1");
+                    else
+                        bw.write(queue.getFirst());
+                    break;
+                default:
+                    StringTokenizer st = new StringTokenizer(tempConstruction, " ");
+                    st.nextToken();
+                    queue.push(st.nextToken());
+                    j = 1;
+                    break;
+            }
+
+            if(j == 0)
+                bw.newLine();
+
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
