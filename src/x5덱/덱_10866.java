fc3b5c31152ee6063acdff62665c4673e6f07192
kangchanwook <kan0202@naver.com>
2024-09-11 23:39:18 +0900

    [SOLVED] : 덱_덱_10866
    comment: -
    knowledge: -

---
+++ src/x5덱/덱_10866.java
@@ -0,0 +1,79 @@
+package x5덱;
+
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class 덱_10866 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<String> queue = new ArrayDeque<>();
+
+        for(int i = 0; i < N; i++){
+            String tempConstruction = br.readLine();
+            int j = 0;
+
+            switch(tempConstruction){
+                case "pop_front":
+                    if (!queue.isEmpty())
+                        bw.write(queue.pollLast());
+                    else
+                        bw.write("-1");
+                    break;
+                case "pop_back":
+                    if (!queue.isEmpty())
+                        bw.write(queue.pollFirst());
+                    else
+                        bw.write("-1");
+                    break;
+                case "size":
+                    bw.write(String.valueOf(queue.size()));
+                    break;
+                case "empty":
+                    if(queue.isEmpty())
+                        bw.write("1");
+                    else
+                        bw.write("0");
+                    break;
+                case "front":
+                    if(queue.isEmpty())
+                        bw.write("-1");
+                    else
+                        bw.write(queue.getLast());
+                    break;
+                case "back":
+                    if(queue.isEmpty())
+                        bw.write("-1");
+                    else
+                        bw.write(queue.getFirst());
+                    break;
+                default:
+                    StringTokenizer st = new StringTokenizer(tempConstruction, " ");
+
+                    if(st.nextToken().equals("push_back"))
+                        queue.push(st.nextToken());
+                    else
+                        queue.addLast(st.nextToken());
+
+
+                    j = 1;
+                    break;
+            }
+
+            if(j == 0)
+                bw.newLine();
+
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
---
+++ src/x5덱/회전하는큐_1021.java
@@ -0,0 +1,42 @@
+package x5덱;
+
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class 회전하는큐_1021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int sizeOfQueue = Integer.parseInt(st.nextToken());
+        int numOfTarget = Integer.parseInt(st.nextToken());
+        Deque<String> deque = new ArrayDeque<>();
+
+
+        for(int i = 0; i < sizeOfQueue; i++){
+            deque.push(String.valueOf(i + 1));
+        }
+
+        st = new StringTokenizer(br.readLine(), " ");
+        Long result = 0L;
+
+        for(int i = 0; i < numOfTarget; i++){
+            String target = st.nextToken();
+            while(true){
+                if(deque.getLast().equals(target)){
+                    deque.pollLast();
+                    break;
+                }
+            }
+        }
+
+
+
+
+    }
+
+}
