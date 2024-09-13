61a892b503c2a1c83cc6be6b2e2b71e4a0a37bf4
chanwookK <kan0202@naver.com>
2024-09-13 18:34:47 +0900

    [SOLVED] : 스택_오아시스재결합_3015
    comment: -
    knowledge: -

--- src/x3스택/오아시스재결합_3015.java
+++ src/x3스택/오아시스재결합_3015.java
@@ -1,85 +1,71 @@
 package x3스택;
 
-import jdk.jfr.Unsigned;
-
 import java.io.*;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Queue;
 
 public class 오아시스재결합_3015 {
+
     public static void main(String[] args) throws IOException {
 
+        //reader writer 선언
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-
         Deque<Element> stack = new ArrayDeque<>();
-
-        Long temp = 0L;
-        Long tempPop = -1L;
         Long result = 0L;
 
+        //첫번째 element 삽입
+        stack.push(new Element(br.readLine()));
 
-        for(int i = 0; i < N; i++){
+        for(int i = 1; i < N; i++){
 
-            Element presentElement = new Element(Long.parseLong(br.readLine()), false);
+            Element next = new Element(br.readLine());
 
-            while(true){
-                if(stack.isEmpty()){
-                    stack.push(presentElement);
+            while(true) {
+
+                if (stack.isEmpty()) {
+                    stack.push(next);
                     break;
                 }
 
-                Element peekElement = stack.peek();
+                Element peek = stack.peek();
 
-                if(peekElement.height > presentElement.height){
-                    stack.push(presentElement);
-                    result += stack.size() - 1L - temp;
-                    presentElement.isIncreasedTemp = true;
-                    temp++;
-                    tempPop = -1L;
+                if (Integer.parseInt(peek.height) > Integer.parseInt(next.height)) {
+                    result += 1;
+                    stack.push(next);
                     break;
-                }
-                else if(peekElement.height == presentElement.height){
-                    stack.push(presentElement);
-                    result += stack.size() - 1L;
-                    presentElement.isIncreasedTemp = true;
-                    temp++;
-                    tempPop = -1L;
-                    break;
-                }
-                else{
-                    Element presentPopData = stack.pop();
+                } else if (Integer.parseInt(peek.height) <= Integer.parseInt(next.height)) {
 
-                    if(tempPop != presentPopData.height)
-                        result += 1L;
-                    if(presentPopData.isIncreasedTemp)
-                        temp--;
+                    if(peek.height.equals(next.height)){
+                        next.cumulative += peek.cumulative;
+                    }
 
-                    tempPop = presentPopData.height;
+                    result += peek.cumulative;
+                    stack.pop();
                 }
             }
 
         }
-
         bw.write(String.valueOf(result));
-        bw.flush();
         bw.close();
         br.close();
-
     }
 
-    public static class Element{
-        Long height;
-        boolean isIncreasedTemp;
 
-        Element(Long height, boolean isIncreasedTemp){
+    public static class Element {
+        String height;
+        Long cumulative;
+
+        public Element(String height) {
             this.height = height;
-            this.isIncreasedTemp = isIncreasedTemp;
-
+            this.cumulative = 1L;
         }
-
+        public Element(String height, Long cumulative) {
+            this.height = height;
+            this.cumulative = cumulative;
+        }
     }
+
 }
