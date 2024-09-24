7ca230c7a09353c6cd361fb27ed1e2e6cf5854b4
chanwookK <kan0202@naver.com>
2024-09-25 03:19:28 +0900

    [SOLVED] : 컴공챌린지_1주차
    comment: 네번째점, 별찍기2, 블랙잭, 이천칠년
    knowledge: -

---
+++ src/컴공백준챌린지/일주차/네번째점_3009.java
@@ -0,0 +1,39 @@
+package 컴공백준챌린지.일주차;
+
+import java.io.*;
+import java.util.*;
+
+public class 네번째점_3009 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] x = new String[3];
+        String[] y = new String[3];
+        String rx;
+        String ry;
+
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            x[i] = st.nextToken();
+            y[i] = st.nextToken();
+        }
+
+        if(x[0].equals(x[1]))
+            rx= x[2];
+        else if(x[1].equals(x[2]))
+            rx= x[0];
+        else
+            rx= x[1];
+
+
+        if(y[0].equals(y[1]))
+            ry= y[2];
+        else if(y[1].equals(y[2]))
+            ry = y[0];
+        else
+            ry= y[1];
+
+        System.out.println(rx+" " + ry);
+
+    }
+}
